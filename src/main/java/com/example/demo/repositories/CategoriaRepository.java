3c37d307a9c2c882d4e4a2b2c939fa895a5c0308
Gustavo Mendes <gustavoomendesoliveira@gmail.com>
2020-10-20 01:48:19 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/example/demo/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repositories;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.*;
+import com.example.demo.domain.*;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer>{
+	
+	
+	
+}

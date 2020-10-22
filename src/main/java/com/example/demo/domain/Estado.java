80698592e09091118085df32d4893cf52a82a5df
Gustavo Mendes <gustavoomendesoliveira@gmail.com>
2020-10-22 03:35:38 -0300

    Adicionando cliente, tipo, endereço e telefones

--- src/main/java/com/example/demo/domain/Estado.java
+++ src/main/java/com/example/demo/domain/Estado.java
@@ -3,8 +3,8 @@ package com.example.demo.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.*;
+
 @Entity
 public class Estado implements Serializable{
 
